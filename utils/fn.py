0c784311ba0d09c303cb545a5fa6d209ec666a1a
CMGS <ilskdw@gmail.com>
2014-05-08 14:24:54 +0800

    Revert "iterate json obj"
    This reverts commit c43ef3719fae7c40343fcc612d5c073d87fd60a4.

--- utils/fn.py
+++ utils/fn.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 #coding:utf-8
 
-from json import JSONEncoder
 from common import dbs_mod, dbs_len
 
 def get_node(id):
@@ -10,7 +9,4 @@ def get_node(id):
 
     id %= dbs_len
     return id
-
-def json(data):
-    return JSONEncoder.iterencode(data)
 
