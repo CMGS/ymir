1e8bbeace5ff3fd4fb7853db7ea0a89d6ba23cbd
CMGS <ilskdw@gmail.com>
2014-05-09 09:16:28 +0800

    output sys info in sys handler, iter json obj

---
+++ utils/ijson.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#coding:utf-8
+
+from json import _default_encoder, JSONEncoder
+
+def dump(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
+        allow_nan=True, cls=None, indent=None, separators=None,
+        encoding='utf-8', default=None, **kw):
+    # cached encoder
+    if (not skipkeys and ensure_ascii and
+        check_circular and allow_nan and
+        cls is None and indent is None and separators is None and
+        encoding == 'utf-8' and default is None and not kw):
+        iterable = _default_encoder.iterencode(obj)
+    else:
+        if cls is None:
+            cls = JSONEncoder
+        iterable = cls(skipkeys=skipkeys, ensure_ascii=ensure_ascii,
+            check_circular=check_circular, allow_nan=allow_nan, indent=indent,
+            separators=separators, encoding=encoding,
+            default=default, **kw).iterencode(obj)
+    # could accelerate with writelines in some versions of Python, at
+    # a debuggability cost
+    for chunk in iterable:
+        yield chunk
+
