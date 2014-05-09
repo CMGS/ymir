74633c655371d46d5a13277624eeaaf25513eeab
CMGS <ilskdw@gmail.com>
2014-05-09 13:42:46 +0800

    test get block

--- tests/test_block.py
+++ tests/test_block.py
@@ -6,6 +6,7 @@ import falcon
 from falcon import testing
 
 from app import app
+from query.site import block, get_site_by_token
 
 from tests.base import is_iter
 from tests.base import TEST_TOKEN
@@ -39,22 +40,35 @@ class TestBlock(CommentTestBase):
 
 
     def test_rm_block(self):
-        from query.site import block, get_site_by_token
-
         site = get_site_by_token(TEST_TOKEN)
-        block = block(site, '192.168.1.1')
+        nblock = block(site, '192.168.1.1')
 
         response = app(
             testing.create_environ(
                 path='/block', \
                 method='DELETE', \
-                body=json.dumps({'id': block.id, 'token': TEST_TOKEN}), \
+                body=json.dumps({'id': nblock.id, 'token': TEST_TOKEN}), \
             ), \
             self.mock, \
         )
 
         self.assertEqual(falcon.HTTP_200, self.mock.status)
+        self.assertFalse(response)
 
     def test_get_block(self):
-        pass
+        response = app(
+            testing.create_environ(
+                path='/block', \
+                method='GET', \
+                body=json.dumps({'page': 1, 'token': TEST_TOKEN, 'num': 1}), \
+            ), \
+            self.mock, \
+        )
+
+        self.assertTrue(is_iter(response))
+        self.assertEqual(falcon.HTTP_200, self.mock.status)
+        data = json.loads(''.join(response))
+        self.assertIsInstance(data, list)
+
+        self._test_bad_request('/block', 'GET', {'page': -1, 'token': TEST_TOKEN, 'num': 1})
 
