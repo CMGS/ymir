a026ed7e75414b166736d08dff7bcc5f85366e2a
CMGS <ilskdw@gmail.com>
2014-05-09 09:40:03 +0800

    refactor test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,23 +8,13 @@ from utils import ijson
 
 class TestFn(TestCase):
 
-    def setUp(self):
-        super(TestFn, self).setUp()
-        self.mod = fn.dbs_mod
-        self.len = fn.dbs_len
-
-    def tearDown(self):
-        super(TestFn, self).tearDown()
-        fn.dbs_mod = self.mod
-        fn.dbs_len = self.len
-
     def test_get_node(self):
-        fn.dbs_mod = 10
-        fn.dbs_len = 1
+        self.patch(fn, 'dbs_mod', 10)
+        self.patch(fn, 'dbs_len', 1)
         assert fn.get_node(1) == 0
         assert fn.get_node(2) == 0
-        fn.dbs_mod = 10
-        fn.dbs_len = 2
+        self.patch(fn, 'dbs_mod', 10)
+        self.patch(fn, 'dbs_len', 2)
         assert fn.get_node(1) == 1
         assert fn.get_node(4) == 0
 
